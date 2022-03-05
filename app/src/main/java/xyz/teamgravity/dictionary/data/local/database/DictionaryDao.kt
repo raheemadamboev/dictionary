b78cd7e0d8af527827991835a36873760a4adc97
raheemadamboev <vividricemaryandmax@gmail.com>
2022-03-05 16:13:00 +0500

    dictionary dao setup

---
+++ app/src/main/java/xyz/teamgravity/dictionary/data/local/database/DictionaryDao.kt
@@ -0,0 +1,32 @@
+package xyz.teamgravity.dictionary.data.local.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import xyz.teamgravity.dictionary.data.local.entity.WordInfoEntity
+
+@Dao
+interface DictionaryDao {
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Insert
+    ///////////////////////////////////////////////////////////////////////////
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertWordInfos(info: WordInfoEntity)
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Delete
+    ///////////////////////////////////////////////////////////////////////////
+
+    @Query("DELETE FROM ${DictionaryConst.TABLE_WORD_INFO} WHERE word IN(:words)")
+    suspend fun deleteWordInfos(words: List<String>)
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Get
+    ///////////////////////////////////////////////////////////////////////////
+
+    @Query("SELECT * FROM ${DictionaryConst.TABLE_WORD_INFO} WHERE word LIKE '%' || :word || '%'")
+    suspend fun getWordInfos(word: String): List<WordInfoEntity>
+}
