f42b494ac5fd22a8489ff02289ae915fbc6f865e
raheemadamboev <vividricemaryandmax@gmail.com>
2022-03-05 15:23:17 +0500

    json parser interface and its gson parser implementation

---
+++ app/src/main/java/xyz/teamgravity/dictionary/core/util/GsonParser.kt
@@ -0,0 +1,17 @@
+package xyz.teamgravity.dictionary.core.util
+
+import com.google.gson.Gson
+import java.lang.reflect.Type
+
+class GsonParser : JsonParser {
+
+    private val gson = Gson()
+
+    override fun <T> fromJson(json: String, type: Type): T? {
+        return gson.fromJson(json, type)
+    }
+
+    override fun <T> toJson(obj: T, type: Type): String? {
+        return gson.toJson(obj, type)
+    }
+}
---
+++ app/src/main/java/xyz/teamgravity/dictionary/core/util/JsonParser.kt
@@ -0,0 +1,10 @@
+package xyz.teamgravity.dictionary.core.util
+
+import java.lang.reflect.Type
+
+interface JsonParser {
+
+    fun <T> fromJson(json: String, type: Type): T?
+
+    fun <T> toJson(obj: T, type: Type): String?
+}
